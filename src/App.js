d208100e87516dccceb4a3b5f6f9d5bcbbe4c69b
kev <45981465+abbo-1@users.noreply.github.com>
2020-06-10 18:20:59 -0500

    buttons on front page

--- src/App.js
+++ src/App.js
@@ -253,19 +253,20 @@ const HomepageLayout = () => (
       <Grid celled='internally' columns='equal' stackable>
         <Grid.Row textAlign='center'>
           <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+            <button class ="clydeButton" >
             <Header as='h3' style={{ fontSize: '2em' }}>
-              "What a Company"
+              Clyde James Richardson
             </Header>
-            <p style={{ fontSize: '1.33em' }}>That is what they all say about us</p>
+            <p style={{ fontSize: '1.33em' }}>A Biography: 1911 - 2002</p>
+            </button>
           </Grid.Column>
           <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-            <Header as='h3' style={{ fontSize: '2em' }}>
-              "I shouldn't have gone with their competitor."
+            <button class="jonesButton">
+          <Header as='h3' style={{ fontSize: '2em' }}>
+              Robert Erskine Jones
             </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              <Image avatar src='/images/avatar/large/nan.jpg' />
-              <b>Nan</b> Chief Fun Officer Acme Toys
-            </p>
+            <p style={{ fontSize: '1.33em' }}>A Biography: 1922 - 1962</p>
+            </button>
           </Grid.Column>
         </Grid.Row>
       </Grid>
