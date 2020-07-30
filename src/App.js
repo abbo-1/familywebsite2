6fdff995b59cbeef2b64f75787949b1918702cdd
kev <45981465+abbo-1@users.noreply.github.com>
2020-07-30 17:52:18 -0500

    banner finally working. ad as well

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
+import 'semantic-ui-css/semantic.min.css'
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -346,22 +348,8 @@ const HomepageLayout = () => (
                 <List.Item as='a'>Gazebo Plans</List.Item>
               </List>
             </Grid.Column>
-            <Grid.Column width={3}>
-              <Header inverted as='h4' content='Services' />
-              <List link inverted>
-                <List.Item as='a'>Banana Pre-Order</List.Item>
-                <List.Item as='a'>DNA FAQ</List.Item>
-                <List.Item as='a'>How To Access</List.Item>
-                <List.Item as='a'>Favorite X-Men</List.Item>
-              </List>
-            </Grid.Column>
             <Grid.Column width={7}>
-              <Header as='h4' inverted>
-                Footer Header
-              </Header>
-              <p>
-                Extra space for a call to action inside the footer that could help re-engage users.
-              </p>
+              <div class="ui leaderboard test ad" data-text="The Far Side of Silence"></div>
             </Grid.Column>
           </Grid.Row>
         </Grid>
