e12484ab48bceb849c94001b2ba725ff0a14cd88
kev <45981465+abbo-1@users.noreply.github.com>
2020-08-04 16:52:13 -0500

    redux implementation

--- src/store.js
+++ src/store.js
@@ -1,13 +1,12 @@
 import { createStore } from 'redux';
 
 const initialState = {
-    drinkChoice: "none"
+    articleChoice: "none"
 }
 
 const store = createStore((state = initialState, action) => { 
             console.log("Action:", action)
-        if (action.type === "CHANGE_INFO" ) {
-
+        if (action.type === "DISPLAY_CLYDEWW2" ) {
             return { ...state, drinkChoice : action.drinkChoice };
         } 
         else if (action.type === "CHANGE_PHOTO") {
@@ -20,5 +19,5 @@ const store = createStore((state = initialState, action) => {
         return state;
     }
 });
-//functional component
+
 export default store;
