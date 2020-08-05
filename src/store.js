7b2e431420a392225f70758c38673ca22dca6d20
kev <45981465+abbo-1@users.noreply.github.com>
2020-08-05 17:54:17 -0500

    still working with redux

--- src/store.js
+++ src/store.js
@@ -7,9 +7,9 @@ const initialState = {
 const store = createStore((state = initialState, action) => { 
             console.log("Action:", action)
         if (action.type === "DISPLAY_CLYDEWW2" ) {
-            return { ...state, drinkChoice : action.drinkChoice };
+            return { ...state, articleChoice : action.articleChoice };
         } 
-        else if (action.type === "CHANGE_PHOTO") {
+        else if (action.type === "DISPLAY_CLYDEANDEDNA") {
 
             console.log("in reducer manufacturer") 
             return { ...state, manufacturer : action.value };
